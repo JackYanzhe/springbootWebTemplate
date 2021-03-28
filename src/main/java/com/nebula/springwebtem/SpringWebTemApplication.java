42d911883001d8c8209da2a687f95d39f0cd1de0
JackYanzhe <532968876@qq.com>
2021-03-28 13:24:39 +0800

    springbootWeb模板

---
+++ src/main/java/com/nebula/springwebtem/SpringWebTemApplication.java
@@ -0,0 +1,13 @@
+package com.nebula.springwebtem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebTemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebTemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nebula/springwebtem/SpringWebTemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nebula.springwebtem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebTemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
